cfcf694736467a6154f3a479ecced73c74f5db1a
IvanGerman <ivgvi@mail.ru>
2023-04-24 00:25:43 +0700

    feat: start to implement navbar, install material ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="left">l</div>
+        <div className="center">c</div>
+        <div className="right">r</div>
+      </div>
+    </div>
   )
 }
