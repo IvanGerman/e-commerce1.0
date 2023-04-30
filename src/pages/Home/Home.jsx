83d9c6f5c5dab5e2d3d9ef067ab3d6619f509e93
IvanGerman <ivgvi@mail.ru>
2023-04-30 11:24:49 +0700

    feat: creating contact component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import './Home.scss';
 import { Slider } from '../../components/Slider/Slider'
 import { FeaturedProducts } from '../../components/FeaturedProducts/FeaturedProducts';
 import { Categories } from '../../components/Categories/Categories';
+import { Contact } from '../../components/Contact/Contact';
 
 export const Home = () => {
   return (
@@ -12,7 +13,7 @@ export const Home = () => {
       <FeaturedProducts type="featured"/>
       <Categories/>
       <FeaturedProducts type="trending"/>
-
+      <Contact/>
     </div>
   )
 }
