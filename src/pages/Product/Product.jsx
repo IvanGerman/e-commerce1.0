b223df4dd3b9255f4915944be2d8315dabb1c8da
IvanGerman <ivgvi@mail.ru>
2023-05-01 19:08:00 +0700

    feat: start to implement single product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 import './Product.scss';
 
 export const Product = () => {
+
+  const images = [];
+  
   return (
-    <div>Product</div>
+    <div>
+      <div className="left">
+          <div className="images">
+              <img  src='' alt="" />
+              <img  src='' alt="" />
+          </div>
+          <div className="mainImg">
+            <img  src='' alt="" />
+          </div>
+      </div>
+
+      <div className="right">
+
+      </div>
+    </div>
   )
 }
